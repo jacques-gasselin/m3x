5d2c4b78f1a882549c586384476b1fbefb1a5b75
jyrkis <jyrkis@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2008-06-18 12:25:12 +0000

    Added.

---
+++ tools/src/m3x/m3g/objects/Group.java
@@ -0,0 +1,32 @@
+package m3x.m3g.objects;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import m3x.m3g.M3GSerializable;
+import m3x.m3g.objects.Object3D.UserParameter;
+import m3x.m3g.primitives.ObjectIndex;
+
+public class Group extends Object3D implements M3GSerializable 
+{
+	private final ObjectIndex[] children;
+
+	public Group(ObjectIndex[] animationTracks, UserParameter[] userParameters,
+			ObjectIndex[] children) 
+	{
+		super(animationTracks, userParameters);
+		assert(children != null);
+		this.children = children;
+	}
+
+	@Override
+	public void serialize(DataOutputStream dataOutputStream, String m3gVersion)
+			throws IOException 
+	{
+		super.serialize(dataOutputStream, m3gVersion);
+		for (int i = 0; i < this.children.length; i++)
+		{
+			this.children[i].serialize(dataOutputStream, m3gVersion);
+		}
+	}	
+}
