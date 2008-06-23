81bf7eed77ee1fafcd0e1c57e183aced9dd2acac
jyrkis <jyrkis@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2008-06-23 10:27:35 +0000

    Added.

---
+++ tools/src/m3x/m3g/objects/Sprite.java
@@ -0,0 +1,48 @@
+package m3x.m3g.objects;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import m3x.m3g.M3GSerializable;
+import m3x.m3g.M3GSupport;
+import m3x.m3g.objects.Object3D.UserParameter;
+import m3x.m3g.primitives.ObjectIndex;
+
+public class Sprite extends Object3D implements M3GSerializable
+{
+  private final ObjectIndex image;
+  private final ObjectIndex appearance;
+  private final boolean isScaled;
+  private final int cropX;
+  private final int cropY;
+  private final int cropWidth;
+  private final int cropHeight;
+  
+  public Sprite(ObjectIndex[] animationTracks, UserParameter[] userParameters,
+      ObjectIndex image, ObjectIndex appearance, boolean isScaled, int cropX,
+      int cropY, int cropWidth, int cropHeight)
+  {
+    super(animationTracks, userParameters);
+    this.image = image;
+    this.appearance = appearance;
+    this.isScaled = isScaled;
+    this.cropX = cropX;
+    this.cropY = cropY;
+    this.cropWidth = cropWidth;
+    this.cropHeight = cropHeight;
+  }
+
+  @Override
+  public void serialize(DataOutputStream dataOutputStream, String m3gVersion)
+      throws IOException
+  {
+    super.serialize(dataOutputStream, m3gVersion);
+    this.image.serialize(dataOutputStream, m3gVersion);
+    this.appearance.serialize(dataOutputStream, m3gVersion);
+    dataOutputStream.writeBoolean(this.isScaled);
+    dataOutputStream.write(M3GSupport.swapBytes(this.cropX));
+    dataOutputStream.write(M3GSupport.swapBytes(this.cropY));
+    dataOutputStream.write(M3GSupport.swapBytes(this.cropWidth));
+    dataOutputStream.write(M3GSupport.swapBytes(this.cropHeight));
+  }
+}
