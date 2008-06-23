b33336a4feb85c28a4e083dc36e4d8ad308d58f3
jyrkis <jyrkis@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2008-06-23 10:48:58 +0000

    Added.

---
+++ tools/src/m3x/m3g/objects/IndexBuffer.java
@@ -0,0 +1,14 @@
+package m3x.m3g.objects;
+
+import m3x.m3g.primitives.ObjectIndex;
+import m3x.m3g.M3GSerializable;
+
+public abstract class IndexBuffer extends Object3D implements M3GSerializable
+{
+
+  public IndexBuffer(ObjectIndex[] animationTracks,
+      UserParameter[] userParameters)
+  {
+    super(animationTracks, userParameters);
+  }
+}
