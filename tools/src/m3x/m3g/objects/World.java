19d25540dda5a20a14b68adcf00cb051856985cf
jyrkis <jyrkis@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2008-06-23 11:12:38 +0000

    d

---
+++ tools/src/m3x/m3g/objects/World.java
@@ -0,0 +1,34 @@
+package m3x.m3g.objects;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import m3x.m3g.M3GSerializable;
+import m3x.m3g.primitives.Matrix;
+import m3x.m3g.primitives.ObjectIndex;
+
+public class World extends Group implements M3GSerializable
+{
+  private final ObjectIndex activeCamera;
+  private final ObjectIndex background;
+  
+  public World(ObjectIndex[] animationTracks, UserParameter[] userParameters,
+      Matrix transform, boolean enableRendering, boolean enablePicking,
+      byte alphaFactor, int scope, ObjectIndex[] children,
+      ObjectIndex activeCamera, ObjectIndex background)
+  {
+    super(animationTracks, userParameters, transform, enableRendering,
+        enablePicking, alphaFactor, scope, children);
+    this.activeCamera = activeCamera;
+    this.background = background;
+  }
+
+  @Override
+  public void serialize(DataOutputStream dataOutputStream, String m3gVersion)
+      throws IOException
+  {
+    super.serialize(dataOutputStream, m3gVersion);
+    this.activeCamera.serialize(dataOutputStream, m3gVersion);
+    this.background.serialize(dataOutputStream, m3gVersion);
+  }
+}
