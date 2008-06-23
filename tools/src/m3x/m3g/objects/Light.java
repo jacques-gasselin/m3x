6dfa3441a8821ae97aa9a8bdafefcbae4ae4042a
jyrkis <jyrkis@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2008-06-23 11:00:23 +0000

    Added.

---
+++ tools/src/m3x/m3g/objects/Light.java
@@ -0,0 +1,62 @@
+package m3x.m3g.objects;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import m3x.m3g.M3GSerializable;
+import m3x.m3g.M3GSupport;
+import m3x.m3g.objects.Object3D.UserParameter;
+import m3x.m3g.primitives.ColorRGB;
+import m3x.m3g.primitives.Matrix;
+import m3x.m3g.primitives.ObjectIndex;
+
+public class Light extends Node implements M3GSerializable 
+{
+	public final static byte MODE_AMBIENT = (byte)128;
+	public final static byte MODE_DIRECTIONAL = (byte)129;
+	public final static byte MODE_OMNI = (byte)130;
+	public final static byte MODE_SPOT = (byte)131;
+	
+	private final float attenuationConstant;
+	private final float attenuationLinear;
+	private final float attenuationQuadratic;
+	private final ColorRGB color;
+	private final byte mode;
+	private final float intensity;
+	private final float spotAngle;
+	private final float spotExponent;
+	
+	public Light(ObjectIndex[] animationTracks, UserParameter[] userParameters,
+			Matrix transform, boolean enableRendering, boolean enablePicking,
+			byte alphaFactor, int scope, float attenuationConstant,
+			float attenuationLinear, float attenuationQuadratic,
+			ColorRGB color, byte mode, float intensity, float spotAngle,
+			float spotExponent) 
+	{
+		super(animationTracks, userParameters, transform, enableRendering,
+				enablePicking, alphaFactor, scope);
+		this.attenuationConstant = attenuationConstant;
+		this.attenuationLinear = attenuationLinear;
+		this.attenuationQuadratic = attenuationQuadratic;
+		this.color = color;
+		this.mode = mode;
+		this.intensity = intensity;
+		this.spotAngle = spotAngle;
+		this.spotExponent = spotExponent;
+	}
+
+  @Override
+  public void serialize(DataOutputStream dataOutputStream, String m3gVersion)
+      throws IOException
+  {
+    super.serialize(dataOutputStream, m3gVersion);
+    dataOutputStream.write(M3GSupport.swapBytes(this.attenuationConstant));
+    dataOutputStream.write(M3GSupport.swapBytes(this.attenuationLinear));
+    dataOutputStream.write(M3GSupport.swapBytes(this.attenuationQuadratic));
+    this.color.serialize(dataOutputStream, m3gVersion);
+    dataOutputStream.write(this.mode);
+    dataOutputStream.write(M3GSupport.swapBytes(this.intensity));
+    dataOutputStream.write(M3GSupport.swapBytes(this.spotAngle));
+    dataOutputStream.write(M3GSupport.swapBytes(this.spotExponent));
+  }
+}
