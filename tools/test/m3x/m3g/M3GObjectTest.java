02abd425b1b1fa964ce1d7c5f0dae7dcc81af394
jyrkis <jyrkis@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2008-07-30 08:56:10 +0000

    Added loading of all test data.

--- tools/test/m3x/m3g/M3GObjectTest.java
+++ tools/test/m3x/m3g/M3GObjectTest.java
@@ -15,16 +15,10 @@ public class M3GObjectTest extends TestCase
   {
     try
     {
-      M3GObject object = new M3GObject();
-      DataInputStream dataInputStream = new DataInputStream(new FileInputStream("tools/test/data/teapot.m3g"));
-      object.deserialize(dataInputStream, "1.0");
-      System.out.println(object.getFileIdentifier());
-      System.out.println(object.getHeader());
-      for (M3GTypedObject typedObject : object.getObjects())
-      {
-        assertNotNull(typedObject);
-        System.out.println(typedObject);
-      }
+      loadObject("tools/test/data/teapot.m3g");
+      loadObject("tools/test/data/helloworld.m3g");
+      loadObject("tools/test/data/nokia_on_ice.m3g");
+      loadObject("tools/test/data/otokka_jump2.m3g");
     }
     catch (IOException e)
     {
@@ -33,6 +27,21 @@ public class M3GObjectTest extends TestCase
     catch (M3GException e)
     {
       fail(e.getMessage());
+    }
+  }
+
+  private void loadObject(String filename) throws FileNotFoundException, IOException,
+      M3GException
+  {
+    M3GObject object = new M3GObject();
+    DataInputStream dataInputStream = new DataInputStream(new FileInputStream(filename));
+    object.deserialize(dataInputStream, "1.0");
+    System.out.println(object.getFileIdentifier());
+    System.out.println(object.getHeader());
+    for (M3GTypedObject typedObject : object.getObjects())
+    {
+      assertNotNull(typedObject);
+      System.out.println(typedObject);
     }
   }
 }
