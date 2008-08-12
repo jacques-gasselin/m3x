6fa38b3201aff3a9f1863658add78ff7e069b71f
jyrkis <jyrkis@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2008-08-12 11:28:17 +0000

    Added test.

---
+++ tools/test/m3x/m3g/M3GLoaderTest.java
@@ -0,0 +1,47 @@
+package m3x.m3g;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.util.Arrays;
+
+import junit.framework.TestCase;
+
+public class M3GLoaderTest extends TestCase
+{
+  private final static String[] dataFiles =
+  {
+    "tools/test/data/teapot.m3g",
+    "tools/test/data/helloworld.m3g",
+    "tools/test/data/nokia_on_ice.m3g",
+    "tools/test/data/otokka_jump2.m3g",
+  };
+  
+  public void testLoader()
+  {
+    for (String fileName : dataFiles)
+    {
+      fileTest(fileName);
+    }
+  }
+  
+  private void fileTest(String fileName)
+  {
+    try
+    {
+      FileInputStream fis = new FileInputStream(fileName);
+      int length = fis.available();
+      byte[] fileBytes = new byte[length];
+      fis.read(fileBytes);
+      M3GObject object1 = M3GLoader.load(fileBytes);
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      M3GLoader.save(baos, object1);
+      byte[] serializedBytes = baos.toByteArray();
+      baos.close();
+      assertTrue(Arrays.equals(fileBytes, serializedBytes));
+    }
+    catch (Exception e)
+    {
+      fail(e.getMessage());
+    }
+  }
+}
