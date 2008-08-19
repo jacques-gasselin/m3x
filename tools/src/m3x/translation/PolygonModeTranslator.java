c51f945af023d5268b61fff8931925259b2ce989
jyrkis <jyrkis@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2008-08-19 10:49:49 +0000

    Added enums => M3G constants translaton.

--- tools/src/m3x/translation/PolygonModeTranslator.java
+++ tools/src/m3x/translation/PolygonModeTranslator.java
@@ -8,10 +8,14 @@ import javax.xml.bind.JAXBContext;
 
 import m3x.m3g.FileFormatException;
 import m3x.m3g.objects.Object3D;
+import m3x.m3g.objects.PolygonMode;
 import m3x.m3g.primitives.Matrix;
 import m3x.m3g.primitives.ObjectIndex;
 import m3x.xml.NodeType;
 import m3x.xml.Object3DType;
+import m3x.xml.PolygonCullingModeType;
+import m3x.xml.PolygonShadingModeType;
+import m3x.xml.PolygonWindingModeType;
 
 public class PolygonModeTranslator extends AbstractTranslator
 {
@@ -33,9 +37,9 @@ public class PolygonModeTranslator extends AbstractTranslator
     {
       this.m3gObject = new m3x.m3g.objects.PolygonMode(animationTracks, 
             userParameters, 
-            polygonMode.getCulling().ordinal(),
-            polygonMode.getShading().ordinal(),
-            polygonMode.getWinding().ordinal(),
+            toM3G(polygonMode.getCulling()),
+            toM3G(polygonMode.getShading()),
+            toM3G(polygonMode.getWinding()),
             polygonMode.isTwoSidedLightingEnabled().booleanValue(),
             polygonMode.isLocalCameraLightingEnabled().booleanValue(),
             polygonMode.isPerspectiveCorrectionEnabled().booleanValue());
@@ -45,6 +49,49 @@ public class PolygonModeTranslator extends AbstractTranslator
       throw new IllegalArgumentException(e);
     }
     return this.m3gObject;
+  }
+
+  private int toM3G(PolygonWindingModeType winding)
+  {
+    if (winding.toString().equals(PolygonWindingModeType.CW))
+    {
+      return PolygonMode.WINDING_CW;
+    }
+    if (winding.toString().equals(PolygonWindingModeType.CCW))
+    {
+      return PolygonMode.WINDING_CCW;
+    }
+    throw new IllegalArgumentException(winding.toString());
+  }
+
+  private int toM3G(PolygonShadingModeType shading)
+  {
+    if (shading.toString().equals(PolygonShadingModeType.SMOOTH))
+    {
+      return PolygonMode.SHADE_SMOOTH;
+    }
+    if (shading.toString().equals(PolygonShadingModeType.FLAT))
+    {
+      return PolygonMode.SHADE_FLAT;
+    }
+    throw new IllegalArgumentException(shading.toString());
+  }
+
+  private int toM3G(PolygonCullingModeType culling)
+  {
+    if (culling.toString().equals(PolygonCullingModeType.FRONT))
+    {
+      return PolygonMode.CULL_FRONT;
+    }
+    if (culling.toString().equals(PolygonCullingModeType.BACK))
+    {
+      return PolygonMode.CULL_BACK;
+    }
+    if (culling.toString().equals(PolygonCullingModeType.NONE))
+    {
+      return PolygonMode.CULL_NONE;
+    }
+    throw new IllegalArgumentException(culling.toString());
   }
 
   public Object3DType toXML()
