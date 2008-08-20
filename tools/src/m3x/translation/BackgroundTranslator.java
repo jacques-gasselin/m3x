cfff25254a049a86b8465be6512690c1b7d7ae73
jyrkis <jyrkis@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2008-08-20 12:53:52 +0000

    Added.

---
+++ tools/src/m3x/translation/BackgroundTranslator.java
@@ -0,0 +1,74 @@
+package m3x.translation;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+
+import m3x.m3g.FileFormatException;
+import m3x.m3g.objects.Background;
+import m3x.m3g.objects.Object3D;
+import m3x.m3g.primitives.Matrix;
+import m3x.m3g.primitives.ObjectIndex;
+import m3x.xml.BackgroundRepeatType;
+import m3x.xml.NodeType;
+import m3x.xml.Object3DType;
+
+public class BackgroundTranslator extends AbstractTranslator
+{
+
+  public Object3D toM3G()
+  {
+    if (this.m3gObject != null)
+    {
+      return this.m3gObject;
+    }
+
+    // do translation
+    m3x.xml.Background background = (m3x.xml.Background)this.m3xObject;
+    ObjectIndex[] animationTracks = this.getM3GAnimationTracks();
+    Object3D.UserParameter[] userParameters = new Object3D.UserParameter[0];
+
+    int backgroundIndex = this.searchObjectIndex(this.m3xRoot, background.getImage2DInstance());
+    try
+    {
+      this.m3gObject = new m3x.m3g.objects.Background(animationTracks, 
+          userParameters, 
+          this.translateColorRGBA(background.getBackgroundColor()),
+          new ObjectIndex(backgroundIndex),
+          toM3G(background.getBackgroundImageModeX()),
+          toM3G(background.getBackgroundImageModeY()),
+          background.getCropX().intValue(),
+          background.getCropY().intValue(),
+          background.getCropWidth().intValue(),
+          background.getCropHeight().intValue(),
+          background.isDepthClearEnabled().booleanValue(),
+          background.isColorClearEnabled().booleanValue());
+              
+    }
+    catch (FileFormatException e)
+    {
+      throw new IllegalArgumentException(e);
+    }
+    return this.m3gObject;
+  }
+
+  private int toM3G(BackgroundRepeatType backgroundImageMode)
+  {
+    if (backgroundImageMode.equals(BackgroundRepeatType.BORDER))
+    {
+      return Background.MODE_BORDER;
+    }
+    if (backgroundImageMode.equals(BackgroundRepeatType.REPEAT))
+    {
+      return Background.MODE_REPEAT;
+    }
+    throw new IllegalArgumentException(backgroundImageMode.toString());
+  }
+
+  public Object3DType toXML()
+  {
+    return null;
+  }
+}
