382da75887bbf14e5fe3da83a874745dd224ac50
jyrkis <jyrkis@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2008-08-20 13:36:13 +0000

    Added.

---
+++ tools/src/m3x/translation/AnimationControllerTranslator.java
@@ -0,0 +1,47 @@
+package m3x.translation;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+
+import m3x.m3g.FileFormatException;
+import m3x.m3g.objects.Object3D;
+import m3x.m3g.primitives.Matrix;
+import m3x.m3g.primitives.ObjectIndex;
+import m3x.xml.NodeType;
+import m3x.xml.Object3DType;
+
+public class AnimationControllerTranslator extends AbstractTranslator
+{
+
+  public Object3D toM3G()
+  {
+    if (this.m3gObject != null)
+    {
+      return this.m3gObject;
+    }
+
+    // do translation
+    m3x.xml.AnimationController ac = (m3x.xml.AnimationController)this.m3xObject;
+    ObjectIndex[] animationTracks = this.getM3GAnimationTracks();
+    Object3D.UserParameter[] userParameters = new Object3D.UserParameter[0];
+   
+    this.m3gObject = new m3x.m3g.objects.AnimationController(animationTracks, 
+          userParameters,
+          ac.getSpeed(),
+          ac.getWeight(),
+          ac.getActiveIntervalStart(),
+          ac.getActiveIntervalEnd(),
+          ac.getReferenceSequenceTime(),
+          ac.getReferenceWorldTime());
+
+    return this.m3gObject;
+  }
+
+  public Object3DType toXML()
+  {
+    return null;
+  }
+}
