920638a84ee5c05611955e422fd4eb8963921c15
jyrkis <jyrkis@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2008-08-21 06:00:54 +0000

    Added.

---
+++ tools/src/m3x/translation/AppearanceTranslator.java
@@ -0,0 +1,60 @@
+package m3x.translation;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+
+import m3x.m3g.FileFormatException;
+import m3x.m3g.objects.Object3D;
+import m3x.m3g.primitives.Matrix;
+import m3x.m3g.primitives.ObjectIndex;
+import m3x.xml.NodeType;
+import m3x.xml.Object3DType;
+import m3x.xml.Texture2DInstance;
+
+public class AppearanceTranslator extends AbstractTranslator
+{
+
+  public Object3D toM3G()
+  {
+    if (this.m3gObject != null)
+    {
+      return this.m3gObject;
+    }
+
+    // do translation
+    m3x.xml.Appearance appearance = (m3x.xml.Appearance)this.m3xObject;
+    ObjectIndex[] animationTracks = this.getM3GAnimationTracks();
+    Object3D.UserParameter[] userParameters = new Object3D.UserParameter[0];
+   
+    int compositingModeIndex = searchObjectIndex(this.m3xRoot, appearance.getCompositingModeInstance());
+    int fogIndex = searchObjectIndex(this.m3xRoot, appearance.getFogInstance());
+    int polygonModeIndex = searchObjectIndex(this.m3xRoot, appearance.getPolygonModeInstance());
+    int materialIndex = searchObjectIndex(this.m3xRoot, appearance.getMaterialInstance());
+    List<Texture2DInstance> list = appearance.getTexture2DInstance();
+    ObjectIndex[] textureIndices = new ObjectIndex[list.size()];
+    for (int i = 0; i < list.size(); i++)
+    {
+      Texture2DInstance texture = list.get(i);
+      textureIndices[i] = new ObjectIndex(searchObjectIndex(this.m3xRoot, texture.getRef()));
+    }
+
+    this.m3gObject = new m3x.m3g.objects.Appearance(animationTracks, 
+          userParameters, 
+          appearance.getLayer().byteValue(),
+          new ObjectIndex(compositingModeIndex),
+          new ObjectIndex(fogIndex),
+          new ObjectIndex(polygonModeIndex),
+          new ObjectIndex(materialIndex),
+          textureIndices);
+
+    return this.m3gObject;
+  }
+
+  public Object3DType toXML()
+  {
+    return null;
+  }
+}
