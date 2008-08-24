6c8ae6ff76dfbe0904bd8d48f5c365e20f1a9b9c
jyrkis <jyrkis@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2008-08-24 11:16:42 +0000

    Added.

---
+++ tools/src/m3x/translation/MeshTranslator.java
@@ -0,0 +1,67 @@
+package m3x.translation;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import m3x.m3g.FileFormatException;
+import m3x.m3g.objects.Object3D;
+import m3x.m3g.objects.Mesh.SubMesh;
+import m3x.m3g.primitives.Matrix;
+import m3x.m3g.primitives.ObjectIndex;
+import m3x.xml.NodeType;
+import m3x.xml.Object3DType;
+import m3x.xml.MeshType.Submesh;
+
+public class MeshTranslator extends AbstractTranslator
+{
+
+  public Object3D toM3G()
+  {
+    if (this.m3gObject != null)
+    {
+      return this.m3gObject;
+    }
+
+    // do translation
+    m3x.xml.Mesh mesh = (m3x.xml.Mesh)this.m3xObject;
+    ObjectIndex[] animationTracks = this.getM3GAnimationTracks();
+    m3x.xml.TransformableType transformable = (m3x.xml.TransformableType)mesh;
+    Matrix transform = getM3GTransformMatrix(transformable);
+    Object3D.UserParameter[] userParameters = new Object3D.UserParameter[0];
+  
+    int vertexBufferIndex = searchObjectIndex(this.m3xRoot, mesh.getVertexBufferInstance());
+    List<Submesh> list = mesh.getSubmesh();
+    SubMesh[] subMeshes = new SubMesh[list.size()];
+    for (int i = 0; i < subMeshes.length; i++)
+    {
+      int indexBufferIndex = searchObjectIndex(this.m3xRoot, mesh.getSubmesh().get(i).getTriangleStripArrayInstance());
+      int appearanceIndex = searchObjectIndex(this.m3xRoot, mesh.getSubmesh().get(i).getAppearanceInstance());
+      
+      SubMesh subMesh = new SubMesh(new ObjectIndex(indexBufferIndex), new ObjectIndex(appearanceIndex));
+      subMeshes[i] = subMesh;
+    }
+    try
+    {
+      this.m3gObject = new m3x.m3g.objects.Mesh(animationTracks, 
+          userParameters, 
+          transform,
+          mesh.isRenderingEnabled(),
+          mesh.isPickingEnabled(),
+          (byte)(mesh.getAlphaFactor() * 255.0f + 0.5f),
+          mesh.getScope(),
+          new ObjectIndex(vertexBufferIndex),
+          subMeshes);
+    }
+    catch (FileFormatException e)
+    {
+      throw new IllegalArgumentException(e);
+    }
+    return this.m3gObject;
+  }
+
+  public Object3DType toXML()
+  {
+    return null;
+  }
+}
