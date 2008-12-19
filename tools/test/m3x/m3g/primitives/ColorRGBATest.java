04961560e8268c2978c5ea9dd1814671ee5ed43e
jgasseli <jgasseli@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2008-12-19 08:13:20 +0000

    re-enabled ColorRGBA tests.

--- tools/test/m3x/m3g/primitives/ColorRGBATest.java
+++ tools/test/m3x/m3g/primitives/ColorRGBATest.java
@@ -1,22 +1,41 @@
 package m3x.m3g.primitives;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import m3x.m3g.AbstractTestCase;
+import m3x.m3g.Deserialiser;
+import m3x.m3g.Serialiser;
 
 public class ColorRGBATest extends AbstractTestCase
 {
-  public void testSerializationAndDeserialization()
-  {
-    /*ColorRGB color = new ColorRGB(10, 20, 30);
-    try
-    {   
-      byte[] serialized = M3GSupport.objectToBytes(color);
-      ColorRGB deserialized = (ColorRGB)M3GSupport.bytesToObject(serialized, ColorRGB.class);
-      this.doTestAccessors(color, deserialized);
-    }
-    catch (Exception e)
+    public void testSerializationAndDeserialization()
     {
-      e.printStackTrace();
-      fail(e.getMessage());
-    }*/
-  }  
+        ColorRGBA color = new ColorRGBA(10, 20, 30, 255);
+        try
+        {
+            //Serialise
+            Serialiser serialiser = new Serialiser("1.0", null);
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            serialiser.pushOutputStream(out);
+            color.serialize(serialiser);
+            serialiser.popOutputStream();
+
+            byte[] serialized = out.toByteArray();
+
+            //Deserialise
+            Deserialiser deserialiser = new Deserialiser();
+            ByteArrayInputStream in = new ByteArrayInputStream(serialized);
+            deserialiser.pushInputStream(in);
+            ColorRGBA deserialized = new ColorRGBA();
+            deserialized.deserialize(deserialiser);
+            deserialiser.popInputStream();
+            
+            doTestAccessors(color, deserialized);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
 }
