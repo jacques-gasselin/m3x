09c949a704e722ac5aa731f240b26048b79f5090
jgasseli <jgasseli@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2009-04-28 14:48:29 +0000

    Fixed Header object version code duplication.

--- tools/src/m3x/m3g/primitives/Header.java
+++ tools/src/m3x/m3g/primitives/Header.java
@@ -101,6 +101,13 @@ public class Header implements SectionSerialisable
         return this.version[0] + "." + this.version[1];
     }
 
+    /**Sets the version for the m3g binary file.
+     * The format of the versionstring must be MAJOR.MINOR
+     * With both MAJOR and MINOR being single digits.
+     * For this version of the m3g binary format, MAJOR
+     * must equal '1', MINOR must equal '0'.
+     * @param version
+     */
     public void setVersion(String version)
     {
         if (version.length() != 3)
@@ -120,19 +127,20 @@ public class Header implements SectionSerialisable
             throw new IllegalArgumentException("Minor revision is not a digit");
         }
         //Assume format is B.B
-        this.version[0] = (byte) Character.digit(version.charAt(0), 10);
-        this.version[1] = (byte) Character.digit(version.charAt(2), 10);
+        final int major = Character.digit(version.charAt(0), 10);
+        final int minor = Character.digit(version.charAt(2), 10);
+        setVersion(major, minor);
     }
 
     public void setVersion(int major, int minor)
     {
-        if (major < 1)
+        if (major != 1)
         {
-            throw new IllegalArgumentException("major revision must be >= 1");
+            throw new IllegalArgumentException("major revision must be 1");
         }
-        if (minor < 0)
+        if (minor != 0)
         {
-            throw new IllegalArgumentException("minor revision must be >= 0");
+            throw new IllegalArgumentException("minor revision must be = 0");
         }
         this.version[0] = (byte) major;
         this.version[1] = (byte) minor;
