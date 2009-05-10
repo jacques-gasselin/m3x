95dce911b20a6e401c3ce94f75b39a8e92a379d8
jgasseli <jgasseli@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2009-05-10 02:40:12 +0000

    Added beginnings of a JSR297 work-alike for debugging and API testing.

---
+++ jsr297/src/javax/microedition/m3g/Transform.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2008, Jacques Gasselin de Richebourg
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package javax.microedition.m3g;
+
+/**
+ * @author jgasseli
+ */
+public final class Transform
+{
+    public Transform()
+    {
+        setIdentity();
+    }
+
+    public Transform(Transform transform)
+    {
+        set(transform);
+    }
+
+    private static final void requireNotNull(Transform transform)
+    {
+        if (transform == null)
+        {
+            throw new NullPointerException("transform is null");
+        }
+    }
+
+    private static final void requireNotNull(float[] matrix)
+    {
+        if (matrix == null)
+        {
+            throw new NullPointerException("matrix is null");
+        }
+    }
+    
+    public void add(Transform transform)
+    {
+        requireNotNull(transform);
+
+        throw new UnsupportedOperationException();
+    }
+
+    public float determinant()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public float determinant3x3()
+    {
+        throw new UnsupportedOperationException();
+    }
+
+    public void get(float[] matrix)
+    {
+        requireNotNull(matrix);
+
+        throw new UnsupportedOperationException();
+    }
+
+    //TODO: add all the other stubs
+
+    public void set(float[] matrix)
+    {
+        requireNotNull(matrix);
+
+        throw new UnsupportedOperationException();
+    }
+
+    public void set(Transform transform)
+    {
+        requireNotNull(transform);
+        
+        throw new UnsupportedOperationException();
+    }
+
+    public void setIdentity()
+    {
+        throw new UnsupportedOperationException();
+    }
+}
