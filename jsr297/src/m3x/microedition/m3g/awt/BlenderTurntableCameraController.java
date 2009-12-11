cac8a9b16f745c19100f5cf9639b3f228c7dc160
jgasseli <jgasseli@cf5caa1e-6b40-6f9a-dece-ad9fadf7bc4c>
2009-12-11 23:50:25 +0000

    Added documentation and missing copyright information to BlenderTurntableCameraController.

--- jsr297/src/m3x/microedition/m3g/awt/BlenderTurntableCameraController.java
+++ jsr297/src/m3x/microedition/m3g/awt/BlenderTurntableCameraController.java
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2008-2009, Jacques Gasselin de Richebourg
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package m3x.microedition.m3g.awt;
 
 import java.awt.Component;
@@ -7,7 +34,21 @@ import javax.microedition.m3g.Camera;
 import javax.microedition.m3g.Transform;
 
 /**
- * A turntable controller operating like the one in Blender 2.4+
+ * A turntable camera controller operating like the one in Blender 2.4+
+ *
+ * <p>The middle button, usually the wheel, is the trigger for reading input.
+ * When it is depressed the controller will be affected as follows:
+ * <ul>
+ * <li>Default ->  Orbit, yaw driven by x-axis movement, pitch driven by y-axis movement.</li>
+ * <li>Shift held -> Pan in the screen plane.</li>
+ * <li>Control held -> Dolly in the screen z-axis, driven by y-axis movement.</li>
+ * </ul>
+ * Furthermore, scrolling the mouse wheel also affects the dolly. If there is no
+ * middle button; it can be emulated by holding down Alt and the left mouse
+ * button.</p>
+ *
+ * <p>See the <a href="http://www.blender.org/">blender homepage</a> for more
+ * information</p>
  * 
  * @author jgasseli
  */
